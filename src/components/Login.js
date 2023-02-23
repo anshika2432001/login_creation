f006a9db351d419651b57ee5771f4cc6dd6974bf
Anshika Jain <ansuu100@gmail.com>
2023-02-23 13:40:19 +0530

    next commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,10 +8,10 @@ const Login = () => {
       ];
     
       const loginTypeOption = [
-        {id:"1",loginTypeOptionId:"1",name:"One"},
-        {id:"2",loginTypeOptionId:"1",name:"Two"},
-        {id:"3",loginTypeOptionId:"2",name:"Three"},
-        {id:"4",loginTypeOptionId:"2",name:"Four"}
+        {id:"1",loginTypeOptionId:"1",loginTypeOptionName:"One"},
+        {id:"2",loginTypeOptionId:"1",loginTypeOptionName:"Two"},
+        {id:"3",loginTypeOptionId:"2",loginTypeOptionName:"Three"},
+        {id:"4",loginTypeOptionId:"2",loginTypeOptionName:"Four"}
       ];
     
       const [ loginType, setLoginType] = useState([]);
@@ -31,7 +31,7 @@ const Login = () => {
     <Grid  sx={{bgcolor:'#bbdefb',mx:2,py:"8px"}}>
         <Box sx={{bgcolor: 'primary.main', fontWeight: 'bold',mt: '8px', color:'white', py: '8px', borderRadius: '2px', }}>Login Creation Through Excel</Box>
     </Grid>
-    <Divider color='blue'/>
+    <Divider color='blue' sx={{mx:2}}/>
     <Grid container  columnSpacing={1} sx={{mx: 2, width: "97.5%"}}  >
         <Grid item xs sx={{bgcolor: "#bbdefb", height: "fit-content"}}>
             <Stack direction='row'>
@@ -46,22 +46,28 @@ const Login = () => {
                     </RadioGroup>
                 </FormControl>
             </Stack>
+            
         </Grid>
+        
         <Divider orientation='vertical' color='blue' flexItem />
-        <Grid item xs sx={{bgcolor: "#bbdefb"}} >
+        <Grid item xs sx={{bgcolor: "#bbdefb",pb:"8px", pr:"8px"}} >
             <Box>
             <Typography  sx={{display: "flex", alignItems: "left",fontWeight: 'bold' }}>Department:</Typography>
-            <TextField label="----Select----" select fullWidth sx={{bgcolor: "white"}}>
+            <select   style={{bgcolor:"white", width: "100%", padding: "8px"}}>
+            <option value="0">----Select----</option>
             { loginTypeOptions && loginTypeOptions !== undefined ? loginTypeOptions.map((lt,index) => {
                 return(
-                    <option key={index} value={lt.id}>{lt.name}</option>
+                    <option key={index} value={lt.loginTypeOptionId}>{lt.loginTypeOptionName}</option>
                 )
             }):"No such option"}
-            </TextField>
+            </select>
+            
             </Box>
+            
         </Grid>
+        
         <Divider orientation='vertical' color='blue' flexItem />
-        <Grid item xs sx={{bgcolor: "#bbdefb", height: "fit-content"}} >
+        <Grid item xs sx={{bgcolor: "#bbdefb",pb:"8px", height: "fit-content"}} >
             <Stack direction='row' spacing={2}>
                 <Typography sx={{fontWeight: 'bold'}}>File Upload:</Typography>
                 <a href='#' style={{color:'primary.main'}}>(Download Template)</a>
@@ -70,9 +76,12 @@ const Login = () => {
                 <Button variant='outlined' component="label" sx={{bgcolor: "white", color: "black",textTransform: 'none'}}>Choose File<input type="file" hidden/></Button>
                 <Typography>No file chosen</Typography>
             </Stack>
+            
         </Grid>
+        
+
     </Grid>
-    <Stack my={4} spacing={2} direction='row' sx={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+    <Stack my={7} spacing={2} direction='row' sx={{display:"flex",justifyContent:"center",alignItems:"center"}}>
         <Button variant='contained' sx={{borderRadius: 8, px: 4,textTransform: 'none'}} > Submit</Button>
         <Button variant='contained' sx={{borderRadius: 8, px: 4,textTransform: 'none'}}> Reset</Button>
     </Stack>
